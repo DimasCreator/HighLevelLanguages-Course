e979bfbbaddeb32dcc946ac5ddcf3faaf9ea7827
dimaCreator <72435657+dimaCreator@users.noreply.github.com>
2021-03-17 13:53:54 +0300

    Add Lab1 logic

--- CourseLab/Lab1/Program.cs
+++ CourseLab/Lab1/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace Lab1
 {
@@ -6,7 +8,62 @@ namespace Lab1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //TODO:обработка TextLabaLHL
+            string[] lines = File.ReadAllLines("random.txt");
+            Sorter.LenghtSort(lines);
+            Array.Sort(lines);
+            Sub(lines);
+        }
+
+        public static void Sub(string[] str)
+        {
+            Sorter.LenghtSort(str);
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+            watch.Start();
+
+            //находим первоначальную размерность списка подстрок(максимум возможных подстрок)
+            int subCount = 0;
+            for (int i = str[0].Length; i >= 0; i--)
+            {
+                subCount += i;
+            }
+            //заполняем список, который поддерживает уникальность
+            HashSet<string> subStrings = new HashSet<string>(subCount);
+            for (int i = 1; i <= str[0].Length; i++)
+            {
+                for (int j = 0; j <= str[0].Length - i; j++)
+                {
+                    subStrings.Add(str[0].Substring(j, i));
+                }
+            }
+            //проверяем на вхождение подстрок
+            List<string> commonSubstrings = new List<string>();
+            foreach (var sub in subStrings)
+            {
+                bool flag = true;
+                for (int i = 1; i < str.Length; i++)
+                {
+                    if (str[i].Contains(sub))
+                    {
+                        continue;
+                    }
+                    flag = false;
+                    break;
+                }
+                if (flag)
+                {
+                    commonSubstrings.Add(sub);
+                }
+            }
+            watch.Stop();
+            Console.WriteLine("\n");
+            Console.WriteLine(watch.ElapsedMilliseconds + "\tмс");
+            //вывод
+            Console.WriteLine("\n\nОбщие подстроки\n\n");
+            foreach (var i in commonSubstrings)
+            {
+                Console.WriteLine(i);
+            }
         }
     }
 }
---
+++ CourseLab/Lab1/Sorter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+    public static class Sorter
+    {
+        public static void LenghtSort(string[] lines)
+        {
+            string temp;
+            for (int i = 0; i < lines.Length - 1; i++)
+            {
+                for (int j = i + 1; j < lines.Length; j++)
+                {
+                    if (lines[i].Length > lines[j].Length)
+                    {
+                        temp = lines[i];
+                        lines[i] = lines[j];
+                        lines[j] = temp;
+                    }
+                }
+            }
+        }
+    }
+}
